1c2e3715b79c5dcb61ad426a34c736add48baed8
examen semana 16 - 1 <fabianpachao20@gmail.com>
2023-10-11 19:37:29 -0500

    v04 def. comp.

---
+++ src/componentes/C01componente.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const C01componente = () => {
+  return (
+    <div>
+        <h1>componente 1</h1>
+        primer componente definida...
+      
+    </div>
+  )
+}
+
+export default C01componente
