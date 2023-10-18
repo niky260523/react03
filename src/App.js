e3a7b90f44137cd3a81ad039e0f5073fa1c7b59e
examen semana 16 - 1 <fabianpachao20@gmail.com>
2023-10-18 18:29:44 -0500

    v5.Diseño App.js y AppForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import C01componente from './componentes/C01componente';
+import AppForm from './componentes/AppForm';
 
 function App() {
   return (
-    <div>
-      Hola mundo desde react...
-      <C01componente />
+    <div style={{width:"350px", background:"greenyellow",padding:"10px"}}>
+      < AppForm/>
+      <p>No.1 Juan Manuel     x      A</p>
+      <p>No.1 Rosa Maria      x      A</p>
+
+
     </div>
   );
 }
