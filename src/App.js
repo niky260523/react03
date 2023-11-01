80e6b5efa81c136e6319feb636b2c2646b6e3334
nicole <nicole.aurora0525@gmail.com>
2023-10-31 19:54:25 -0500

    v9 instalacion y configuracion de ruteo

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import { useEffect,useState } from 'react';
-import AppForm from './componentes/AppForm';
-import logo from './logo.svg';
-//import './App.css';
-import C01componente from './componentes/C01componente';
-import{collection,deleteDoc,doc,onSnapshot,query} from 'firebase/firestore';
-import { db } from './componentes/firebase';
-import {deleteApp} from 'firebase/app';
+import React from 'react'
+import { BrowserRouter as Router} from 'react-router-dom'
+import Rutas from './ruteo/Rutas'
+import BarraNavegacion from './ruteo/BarraNavegacion'
 
-function App() {
+const App = () => {
+  return (
+    <div style={{background:"violet"}}>
 
-  /// READ - LECTURA - fnRead /////
-  const [docBD, setDocBD] = useState([]);
-  const fnRead = () => {
-  try {
-    const xColeccionConQuery =query (collection(db,'persona'));
-    const unsuscribete = onSnapshot(xColeccionConQuery, (xDatosBD) => {
-      const xdoc= [];
-      xDatosBD.forEach((doc) => {
-        xdoc.push({id:doc.id,...doc.data()});
-      });
-  setDocBD(xdoc);
-    });
-  } catch (error) {
-  console.error(error);
-    }
-  }
-  fnRead(); //Prueba sin useEffect
-  //useEffect( () =>{fnRead();}, []);
-
-//// DELETE - Eliminar - fnDelete ////
-  const [idActual, setIdActual] = useState("");
-  const fnDelete = (xId) => {
-
-  }
-    return (
-    <div style={{width:"350px", background:"greenyellow",padding:"10px"}}>
-      <h1>sitiocopia2a3 (App.js)</h1>
-      <h3>READ  / DELETE</h3>
-      <AppForm {...{idActual, setIdActual, fnRead}}/>
-      {
-        docBD.map((p) =>
-        <p key={p.id}>
-          No. 1{p.nombre}...
-          <span onclick={() => fnDelete(p.id)}>x</span>
-          ...
-          <span onclick={() => fnDelete(p.id)}>x</span>
-        </p>
-      )
-    } 
-     
-     </div>
-
-  
-  );
+      <h1>App.js</h1>
+      <Router>
+        <BarraNavegacion></BarraNavegacion>
+        <Rutas />
+      </Router>
+      </div>
+  )
 }
 
-export default App;
+export default App
